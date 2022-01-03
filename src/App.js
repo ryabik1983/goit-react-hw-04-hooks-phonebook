261766e16659995b592f2b8ba4b2dbb899bc17a8
ryabik1983 <81900360+ryabik1983@users.noreply.github.com>
2022-01-03 22:54:17 +0200

    hw4 hooks
    hw4 hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,120 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+import ContactForm from './components/ContactForm/ContactForm';
+import Filter from './components/Filter/Filter';
+import ContactsList from './components/ContactList/ContactList';
+// import Modal from './components/Modal/Modal';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+    name: '',
+    number: '',
+    // showModal: false,
+  };
+
+  // toggleModal = () => {
+  //   this.setState(({ showModal }) => ({
+  //     showModal: !showModal,
+  //   }));
+  // };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    console.log(contacts);
+    const parsedContacts = JSON.parse(contacts);
+    console.log(parsedContacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+  // componentDidMount() {
+  //   const contacts = JSON.parse(localStorage.getItem('contacts'));
+  //   console.log('App componentDidMount');
+
+  //   if (contacts) {
+  //     this.setState({ contacts: contacts });
+  //   }
+  // }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log('App Component update');
+    console.log(prevState);
+    console.log(this.state);
+
+    if (this.state.contacts !== prevState.contacts) {
+      console.log('Update Contacts');
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+    // if (prevProps.contacts !== this.state.contacts) {
+    //   console.log('Обновилось поле contacts');
+    //   localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    // }
+  }
+  formSubmitData = ({ name, number }) => {
+    const newItem = { id: nanoid(), name: name, number: number };
+    let isUnique = this.state.contacts.some(el => el.name === name);
+    if (!isUnique) {
+      this.setState(prevStates => ({
+        contacts: [...prevStates.contacts, newItem],
+      }));
+    } else {
+      alert(`${name} is already in contacts`);
+    }
+  };
+
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  renderContacts = () => {
+    const { filter, contacts } = this.state;
+    const toLowerCaseFilter = filter.toLowerCase();
+    return contacts.filter(el =>
+      el.name.toLowerCase().includes(toLowerCaseFilter),
+    );
+  };
+
+  deleteContact = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(el => el.id !== id),
+    }));
+  };
+
+  render() {
+    console.log('App render');
+
+    const { contacts, filter, showModal } = this.state;
+    return (
+      <main className="main">
+        {/* <div>
+          <button type="button" onClick={this.toggleModal}>
+            Open modal
+          </button>
+          {showModal && (
+            <Modal onClose={this.toggleModal}>
+              <h1>Hello!</h1>
+              <p>#lorem ipsum dolor</p>
+              <button type="button" onClick={this.toggleModal}>
+                Close modal
+              </button>
+            </Modal>
+          )}
+        </div> */}
+        <h1 className="title">Phonebook</h1>
+        <ContactForm onSubmit={this.formSubmitData} />
+        <h2 className="title">Contacts</h2>
+        <Filter value={filter} onChange={this.changeFilter} />
+        <ContactsList
+          renderContacts={this.renderContacts()}
+          deleteContact={this.deleteContact}
+        />
+      </main>
+    );
+  }
 }
 
 export default App;
