ee0d71a0272c9df379e3c106f712c3602b58e501
ryabik1983 <81900360+ryabik1983@users.noreply.github.com>
2022-01-06 00:01:09 +0200

    hw4 hooks
    hw4 hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { nanoid } from 'nanoid';
 
 import ContactForm from './components/ContactForm/ContactForm';
