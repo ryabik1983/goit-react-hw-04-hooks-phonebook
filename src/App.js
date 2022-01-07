e4e808b6a27f9fa47f71baef33d8b89f7a45149b
ryabik1983 <81900360+ryabik1983@users.noreply.github.com>
2022-01-07 07:57:23 +0200

    hw4 hooks
    hw4 hooks

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { nanoid } from 'nanoid';
 import ContactForm from './components/ContactForm/ContactForm';
 import Filter from './components/Filter/Filter';
 import ContactsList from './components/ContactList/ContactList';
+import useLocalStorage from './components/uselocalstorage/uselocalstorage';
 // import Modal from './components/Modal/Modal';
 import './App.css';
 
@@ -104,7 +105,7 @@ import './App.css';
         //     </Modal>
         //   )}
         // </div> */}
-        {/* <h1 className="title">Phonebook</h1>
+       /* <h1 className="title">Phonebook</h1>
         <ContactForm onSubmit={this.formSubmitData} />
         <h2 className="title">Contacts</h2>
         <Filter value={filter} onChange={this.changeFilter} />
@@ -117,19 +118,58 @@ import './App.css';
   }
 }
 
-// export default App; */}
-export default function App () {
+// export default App; */
+
+function App () {
 const [filter, setFilter] = useState ('');
-const [contacts, setContacts] = useState ('');
+const [contacts, setContacts] = useLocalStorage('contacts', [
+  // { id: 'id-1', name: 'Vova Putin', number: '459-12-56' },
+  // { id: 'id-2', name: 'Vitya Poroshenko', number: '443-89-12' },
+  // { id: 'id-3', name: 'Vova Lenin', number: '645-17-79' },
+  // { id: 'id-4', name: 'Joseph Stalin', number: '227-91-26' },
+]);
 
-useEffect(() => {
-  console.log('Starting useEffect')
-})
+const formSubmitData = ({ name, number }) => {
+  const newItem = { id: nanoid(), name: name, number: number };
+  let isUnique = contacts.some(el => el.name === name);
+  if (!isUnique) {
+    setContacts(prevContacts => {
+      return [...prevContacts, newItem];
+    });
+  } else {
+    alert(`${name} is already in contacts`);
+  }
+};
 
+const changeFilter = e => {
+  setFilter(e.currentTarget.value);
+};
+
+const renderContacts = () => {
+  const toLowerCaseFilter = filter.toLowerCase();
+  return contacts.filter(el =>
+    el.name.toLowerCase().includes(toLowerCaseFilter),
+  );
+};
+
+const deleteContact = id => {
+  setContacts(prevContacts => {
+    return prevContacts.filter(el => el.id !== id);
+  });
+};
+
+return (
+  <main className="main">
+    <h1 className="title">Phonebook</h1>
+    <ContactForm onSubmit={formSubmitData} />
+    <h2 className="title">Contacts</h2>
+    <Filter value={filter} onChange={changeFilter} />
+    <ContactsList
+      renderContacts={renderContacts()}
+      deleteContact={deleteContact}
+    />
+  </main>
+);
 }
-  //   state = {
-  //     contacts: [],
-  //     filter: '',
-  //     name: '',
-  //     number: '',
-  //     // showModal: false,
+
+export default App;
--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -105,12 +105,12 @@ const ContactForm = ({ onSubmit }) => {
                     className={styles.input}
                     type="tel"
                     name="number"
-                    onChange={handleChange}
-                    value={number}
+                    onChange={handleChange}   
                     pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
                     title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
                     placeholder="067-233-15-14"
                     required
+                    value={number}
                   />
                 </label>
                 <button className={styles.submit} type="submit">
